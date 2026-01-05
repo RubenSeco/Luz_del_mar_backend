b41dcb95b4efd6cf6dbfb1a2bd295758b208166d
Ruben Seco <ruben.seco.rs@gmail.com>
2026-01-05 16:31:26 -0300

    actulizacion 5-1-26

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://luz-del-mar-backend.onrender.com/api',
+    origin: ['https://luzdelmar.netlify.app', "http://localhost:4200"],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
