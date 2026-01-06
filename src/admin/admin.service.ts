70c052492106aa0cc0e138d3b1105a2994347b3f
Ruben Seco <ruben.seco.rs@gmail.com>
2026-01-06 14:01:38 -0300

    actulizacion 6-1-26

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { FileStorageService } from 'src/common/file-storage/file-storage-service.service';
 import { Product } from 'src/common/interfaces/product.interface';
 import path from 'path';
+import { DATA_DIR, DATA_FILES } from 'src/data/data.constants';
 
-const DATA_FILE = path.join(__dirname, '../../products.json');
+const PRODUCTS_FILE = path.join(DATA_DIR, DATA_FILES.products);
 
 @Injectable()
 export class AdminService {
@@ -13,17 +14,17 @@ export class AdminService {
 
 
   getById(id: string): Product {
-    const data = this.fileStorge.read<any>(DATA_FILE);
+    const data = this.fileStorge.read<any>(PRODUCTS_FILE);
     return data.find((product) => product.id === id);
   }
 
 
   update(id: string, product: Product) {
-    const data = this.fileStorge.read(DATA_FILE) as Product[];
+    const data = this.fileStorge.read(PRODUCTS_FILE) as Product[];
     const index = data.findIndex((item: Product) => item.id === id);
     if (index === -1) return null;
     data[index] = { ...data[index], ...product };
-    this.fileStorge.write(DATA_FILE, data);
+    this.fileStorge.write(PRODUCTS_FILE, data);
     return data[index];
   }
 
